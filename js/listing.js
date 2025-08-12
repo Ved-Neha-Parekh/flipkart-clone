17f34cb83f74e5816e2173c12ceaccb381ac9897
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-08-12 08:34:51 +0530

    assured filter done

--- js/listing.js
+++ js/listing.js
@@ -7,3 +7,18 @@ const assured = document.getElementById("assured");
 const fastDelivery = document.getElementById("fastDelivery");
 const sortBy = document.getElementById("sortBy");
 const countText = document.getElementById("countText");
+const assuredCheck = document.getElementById("assured");
+
+assuredCheck.addEventListener("change", () => {
+  const allProducts = document.querySelectorAll(".product");
+  if (assuredCheck.checked) {
+    allProducts.forEach((product) => {
+      product.style.display =
+        product.getAttribute("data-assured") === "1" ? "" : "none";
+    });
+  } else {
+    allProducts.forEach((product) => {
+      product.style.display = "";
+    });
+  }
+});
